926eef2ee075909f747556a84b4fd4e71eba23cc
RaveleT <ravele95@gmail.com>
2026-01-28 14:31:28 +0200

    Refactor app_1.py header and title text

--- app_1.py
+++ app_1.py
@@ -7,12 +7,13 @@ Created on Wed Jan 28 10:57:42 2026
 
 import streamlit as st
 
-st.title("Thendo's First Streamlit App")
+st.title("Thendo's Streamlit App")
 
 st.title("My App Is The Best")
 
 name = st.slider("Pick a number", 1, 100)
 st.write(f"You picked: {name}")
+
 
 
 
