b30728bf52bc7ca9a8c1b9806d49af29c4f76e5e
RaveleT <ravele95@gmail.com>
2026-01-28 14:30:35 +0200

    Change app title to 'my App Is The Best'
    Updated the title of the Streamlit app to reflect a new name.

--- app_1.py
+++ app_1.py
@@ -7,16 +7,12 @@ Created on Wed Jan 28 10:57:42 2026
 
 import streamlit as st
 
-
-st.write("CSS 2026:")
-
 st.title("Thendo's First Streamlit App")
+
+st.title("myy App Is The Best")
 
 name = st.slider("Pick a number", 1, 100)
 st.write(f"You picked: {name}")
 
 
 
-st.header("heading 1")
-
-st.markdown("some text that you can write")
